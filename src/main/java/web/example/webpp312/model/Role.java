75bb66d8e7e2087445a4521115ecbc0b8a63fd1e
MIhail <skuter63@mail.ru>
2021-12-21 11:50:54 +0400

    edit model role- fetch LAZY

--- src/main/java/web/example/webpp312/model/Role.java
+++ src/main/java/web/example/webpp312/model/Role.java
@@ -15,8 +15,7 @@ public class Role implements GrantedAuthority {
     @Column(name = "name")
     private String name;
 
-//
-    @ManyToMany(mappedBy = "roles", fetch = FetchType.EAGER)
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
     private Set<User> users;
 
     public Role() {}
