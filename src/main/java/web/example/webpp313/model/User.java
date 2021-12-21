c1cd961a623961c29fe118c6f503c8d90723f9dc
MIhail <skuter63@mail.ru>
2021-12-21 20:17:20 +0400

    edit User

--- src/main/java/web/example/webpp313/model/User.java
+++ src/main/java/web/example/webpp313/model/User.java
@@ -26,7 +26,7 @@ public class User implements UserDetails {
 
     public User() {}
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name="user_roles", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name="role_id"))
 
